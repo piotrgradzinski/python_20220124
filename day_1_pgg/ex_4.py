6dd9637e14d436e6664ede72d763993c60012346
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-24 16:20:06 +0100

    ex 4

--- day_1_pgg/ex_4.py
+++ day_1_pgg/ex_4.py
@@ -1,7 +1,9 @@
 """
 Napisz program obliczający średnią wartość z podanych przez użytkownika liczb.
-Do przechowywania liczb użyj listy. Pozwól na wprowadzenie maksymalnie 10 liczb.
+Do przechowywania liczb użyj listy. 
 Skorzystaj z funkcji wbudowanej sum().
+
+Pozwól na wprowadzenie maksymalnie 10 liczb.
 
 - zasilanie listy danymi
 - policzenie sredniej
@@ -9,3 +11,21 @@ Skorzystaj z funkcji wbudowanej sum().
 """
 
 
+numbers = []
+
+while len(numbers) < 10:
+# while True:  # wersja z dowolna liczba liczb
+    input_data = input('Podaj liczbe lub KONIEC: ')
+
+    if input_data.lower() == 'koniec':
+        break
+
+    try:
+        number = int(input_data)
+        numbers.append(number)
+    except ValueError:
+        print('Niepoprawna liczba, sprobuj jeszcze raz.')
+        continue
+
+average = sum(numbers) / len(numbers)
+print(f'Srednia z wprowadzonych liczb to {average:.2f}')
