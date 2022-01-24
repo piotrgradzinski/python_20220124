592f9bf78ee3224ed0689d6b57c8c85e08d261b4
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-24 17:04:02 +0100

    slowniki

--- day_1_pgg/basics_collections.py
+++ day_1_pgg/basics_collections.py
@@ -124,3 +124,113 @@ print(my_list)
 after_tax = [x * 0.1 for x in my_list]  # list comprehension
 print(after_tax)
 
+print('-' * 60)
+
+from pprint import pprint
+
+"""
+Slownik (dict)
+- struktura ktora zawiera klucze i wartosci
+"""
+my_dict = {
+    'first_name': 'Piotr',
+    'last_name': 'GG',
+    'shoe_number': 46, 
+    1: 100,
+    2: 200,
+    3: [10, 20, 30, 40],
+    4: {
+        'number': 123
+    },
+    5: None,
+}
+
+print(my_dict)
+pprint(my_dict)
+print(my_dict['first_name'])
+print(my_dict[3])
+print(my_dict[3][1])
+my_dict['address'] = 'ul. Polna 1'
+print(my_dict)
+
+# print(my_dict['company_name'])  # KeyError
+
+print('company_name' in my_dict)
+print('first_name' in my_dict)
+
+if 'company_name' in my_dict:
+    print(my_dict['company_name'])
+
+company_name = my_dict.get('company_name')  # zwroci None jak nie ma takiego klucza w slowniku, nie bedzie wyjatku
+print(company_name)
+
+company_name = my_dict.get('company_name', 'Unknown')
+print(company_name)
+
+print('Piotr' in my_dict.values())
+
+# Standardardowo iterujemy po kluczach
+for key in my_dict:
+    print(key, my_dict[key])
+
+print('---')
+
+# Iterowanie po wartosciach ze slownika
+for value in my_dict.values():
+    print(value)
+
+print('---')
+
+# Iterowanie po kluczach i wartosciach na raz
+# key, value = ('first_name', 'Piotr')
+for key, value in my_dict.items():
+    print(key, value)
+
+print('---')
+
+"""
+Rozpakowywanie kolekcji na wiele zmiennych (unpacking).
+Jezeli wymiery sie nie zgadzaja to bedzie ValueError.
+"""
+a, b = 10, 20
+a, b = (10, 20)
+a, b = [10, 20]
+print(a)
+print(b)
+
+print(my_dict.items())  # items oddaje listę tupli [(klucz, wartosc), (klucz, wartosc), ...]
+
+
+print('-' * 60)
+
+"""
+Co moze byc kluczem w slowniku? Prawie wszystko :)
+- generalnie typy niemutowalne
+- kluczami w slowniku moga byc tylko takie obiekty, ktore posiadaja zaimplementowana metode __hash__
+- tzw. typy hashowalne
+"""
+my_dict = {
+    'first_name': 'Piotr',
+    'last_name': 'GG', 
+    0: [-1, -2, -3, -4],
+    1: [10, 20, 30, 40],
+    3.14: 'liczba ulamkowa',
+    False: 'wartosc False',
+    True: 'wartosc True',
+    (1,2,3): 'tupla',
+    # [10, 20, 30]: 'lista'
+}
+
+pprint(my_dict)
+print(my_dict[(1,2,3)])
+print('ala ma kota'.upper())  # wszystkie operacje na napisach powoduja utworzenie nowego napisu
+
+print('first_name'.__hash__())  # -41554381053546126
+print((0).__hash__())  # 0
+print(3.14.__hash__())  # 322818021289917443
+print(False.__hash__())  # 0
+print((1,2,3).__hash__())  # 529344067295497451
+# print([1,2,3].__hash__())  # lista nie ma metody hash - rzuca wyjatkiem
+
+print(my_dict[0])
+print(my_dict[False])
