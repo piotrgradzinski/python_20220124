94556b61debb1158c02ff7879275eddd747a1f4e
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-24 11:13:07 +0100

    ex 1

--- day_1_pgg/basics.py
+++ day_1_pgg/basics.py
@@ -46,3 +46,59 @@ print(False)
 # None - NULL, wartość oznaczajaca brak wartosci
 print(None)
 
+
+print(10)
+print(10.0)
+print("10")
+
+print(type(10))
+print(type(10.0))
+print(type("10"))
+
+# print("10" * "5")  # can't multiply sequence by non-int of type 'str'
+
+# Operatory
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(11 / 3)
+print(11 // 3)  # // -> dzielenie calkowitoliczbowe, ucina czesc ulamkowa, 
+print(10 ** 3)  # operator potegowania
+print(10 % 3)  # reszta z dzielenia
+
+print("Ala " + "ma kota")  # konkatenacja
+print("Ala " * 3)
+
+# Zmienne
+"""
+- staramy sie pisac po angielsku
+- nazwy zmiennej nie zaczynamy od liczby
+- poszczegolne slowa, w nazwie zmiennej, rozdzielamy _
+- PEP8 - Style guide for Python https://www.python.org/dev/peps/pep-0008/
+- python jest jezykiem slabo typowanym
+- type hinting pozwala nam na okreslenie podpowiedzie, jakiego typu dane chcemy przechowywac w zmiennej, czy przekazywac do funkcji
+"""
+
+first_name:str = "Piotr"  # snake_case
+firstName = "Piotr"  # camelCase w pythonie tak nie nazywamy zmiennych
+print(first_name)
+print(firstName)
+
+first_name = 44
+print(first_name)
+
+# przerabianie typow
+my_number = int("10")
+print(my_number)
+print(type(my_number))
+
+"""
+Obsluga dat
+- wczytujemy dane - np. z csv "2022-01-24 11:05:00"
+- strptime("2022-01-24 11:05:00", "%Y-%m-%d %H:%M:%S") -> obiekt datetime
+- mozemy taki obiekt przerobic na napis poprzez strftime("%Y-%m-%d %H:%M:%S")
+"""
+
+user_age = int(input("Podaj swoj wiek: "))
+print(user_age)
+print(type(user_age))
---
+++ day_1_pgg/ex_1.py
@@ -0,0 +1,14 @@
+"""
+Napisz program obliczający koszt przejazdu z miasta A do B na podstawie podanej 
+przez użytkownika liczby kilometrów, ceny paliwa oraz spalania. 
+Zapytaj użytkownika także o nazwy miejscowości.
+
+Przykładowe komunikaty programu:
+Miasto A: Warszawa
+Miasto B: Gdańsk
+Dystans Warszawa-Gdańsk: 420
+Cena paliwa: 4.55
+Spalanie na 100 km: 5.5
+Koszt przejazdu Warszawa-Gdańsk to 105 PLN
+"""
+
