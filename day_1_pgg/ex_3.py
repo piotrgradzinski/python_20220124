3d693983c126b4b728b50cca860410391077abe0
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-24 16:02:52 +0100

    ex 3 i ex 4

--- day_1_pgg/ex_3.py
+++ day_1_pgg/ex_3.py
@@ -6,6 +6,13 @@ Korzystając z operatora dostępu oraz wycinania wyświetl:
 - co trzeci element
 - co drugi element licząc od końca
 """
-
+#          0   1   2   3   4   5   6   7   8   9 - indeksy
+#          -10 -9  -8  -7  -6  -5  -4  -3  -2  -1 - indeksy ujemne
+#          1   2   3   4   5   6   7   8   9   10 - ktory element?
 numbers = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
 
+print(numbers[1])
+print(numbers[-2])
+print(numbers[2:7])
+print(numbers[::3])
+print(numbers[::-2])
---
+++ day_1_pgg/ex_4.py
@@ -0,0 +1,11 @@
+"""
+Napisz program obliczający średnią wartość z podanych przez użytkownika liczb.
+Do przechowywania liczb użyj listy. Pozwól na wprowadzenie maksymalnie 10 liczb.
+Skorzystaj z funkcji wbudowanej sum().
+
+- zasilanie listy danymi
+- policzenie sredniej
+- wyswietlenie wynikow
+"""
+
+
