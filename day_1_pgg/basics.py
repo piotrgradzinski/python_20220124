a7621bdc8f99305c9cb97e7503d8cf7bd652b3f7
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-24 10:27:52 +0100

    Basics.

---
+++ day_1_pgg/basics.py
@@ -0,0 +1,48 @@
+"""
+Multi-line comment, tzw. docstring
+line1
+line2
+"""
+
+# Comment
+
+# Python data types
+
+# str
+print("Ala ma kota")
+print('Ala ma kota')
+
+# multi-line string literals (literal)
+print('''Ala
+ma 
+kota
+''')
+print("""Ala
+ma
+kota
+""")
+
+# int - nieograczony
+print(3)
+print(-1)
+
+# float 
+print(3.14)
+print(-8.5)
+print(0.5)
+print(.5)
+
+print(3E5)  # notacja naukowa 3*10^5
+
+# liczby zespolone 
+# <czesc rzeczywista>+<czesc urojona>j
+print(3+5j)
+print(type(3+5j))
+
+# bool
+print(True)
+print(False)
+
+# None - NULL, wartość oznaczajaca brak wartosci
+print(None)
+
