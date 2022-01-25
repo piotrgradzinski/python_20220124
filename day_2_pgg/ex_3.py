e2f35aeab1d9d5972a91df22c85a96f441e234a4
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-25 11:36:21 +0100

    ex 3

--- day_2_pgg/ex_3.py
+++ day_2_pgg/ex_3.py
@@ -5,3 +5,18 @@ Sprawdź jak dużo z tych liczb jest liczbami parzystymi
 w zakresie 0-100 - w tym celu skorzystaj z operatora iloczynu.
 """
 
+even_numbers = set(range(0, 101, 2))
+
+user_numbers = set()
+
+while True:
+    number = input('Podaj liczbe calkowita lub KONIEC: ')
+    if number.upper() == 'KONIEC':
+        break
+
+    user_numbers.add(int(number))
+
+
+print(f'Unikalnych liczb = {len(user_numbers)}')
+print(f'Wprowadzone parzyste liczby od 0 do 100 = {even_numbers & user_numbers}')
+print(f'Liczba parzystych liczba od 0 do 100 = {len(even_numbers & user_numbers)}')
