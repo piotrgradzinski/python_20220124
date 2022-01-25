a3b9b37d971e77b2bb779f41fc58679aca71f5b5
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-25 14:58:21 +0100

    cw 4

--- day_2_pgg/cw_4.py
+++ day_2_pgg/cw_4.py
@@ -4,7 +4,7 @@ Napisz funkcję sprawdzającą, czy dana liczba jest liczbą pierwszą.
 - sprawdzanie typu
 - type hinting
 - docstring
-- funkcja zwracala True/False
+- funkcja zwraca True/False
 
 Przykład użycia:
 > is_prime(10)
@@ -19,4 +19,22 @@ def is_prime(number: int) -> bool:
     :param number: Number to be tested if is prime.
     :return:
     """
+
+    if type(number) is not int:
+        # co tutaj zrobic???
+        # return False
+        # return None
+        # return 'Zle dane!!! Przeczytaj dokumentacje!!!'
+        # https://docs.python.org/3/library/exceptions.html#exception-hierarchy
+        raise TypeError('Number is not int.')
+
+    if number <= 1:
+        return False
+
+    for i in range(2, number):
+        if number % i == 0:
+            return False
     
+    return True
+
+
