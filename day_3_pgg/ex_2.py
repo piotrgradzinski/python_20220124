26836e74f48bf0618c2095e44e4e799f4f2816cf
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-26 11:38:35 +0100

    ex 2

---
+++ day_3_pgg/ex_2.py
@@ -0,0 +1,21 @@
+"""
+Zaimplementuj klasę ElectricCar odwzorowującą zachowanie samochodu 
+elektrycznego. 
+Klasa powinna umożliwiać pokonanie zadanego dystansu, który nie może 
+przekroczyć maksymalnego zasięgu zdefiniowanego dla samochodu. 
+Samochód powinien mieć także możliwość naładowania baterii.
+
+https://yuml.me/preview/c83d768b
+
+> car = ElectricCar(100)
+> car.drive(70)
+70
+> car.drive(50)
+30
+> car.drive(50) 
+0
+> car.charge()
+> car.drive(50)
+50
+"""
+
