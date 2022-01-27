79a66f73df2d7120e829a06c8bc6aa32ed4814d3
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-27 13:15:33 +0100

    files

--- day_4_pgg/files_intro.py
+++ day_4_pgg/files_intro.py
@@ -1,4 +1,52 @@
 """
 Obsluga plikow w pythonie
+- otwieramy plik - oddaje nam uchwyt do pliku
+    - jezeli nie ma pliku, nie mozna go znalezc to dostajemy FileNotFoundError
+    - przy otwarciu pliku, moze podac tryb w jakim go otwieramy, standardowo to tryb 'r', czyli do odczytu
+    - https://docs.python.org/3/library/functions.html#open
+    - https://stackoverflow.com/a/16212401/779084 
+    - r - czytanie (domyślny), FileNotFoundError jak nie ma pliku, wskaźnik na początku
+    - w - pisanie, nadpisuje istniejący plik, tworzy nowy plik jak nie ma
+    - a - dodawanie, wskaźnik na końcu pliku, tworzy nowy plik jak nie ma
+    - r+ - czytanie/pisanie, wskaźnik na początku, FileNotFoundError jak nie ma pliku, nie usuwa calej zawartosci pliku
+    - w+ - czytanie/pisanie, czysci zawartosc istniejacego pliku juz przy jego otwarciu, tworzy nowy plik jak nie ma
+    - a+ - czytanie/dodawanie, wskaźnik na końcu pliku, tworzy nowy plik jak nie ma
+    - rb, rb+, wb, wb+, ab, ab+ - analogicznie, ale w trybie binarnym
+
+- wykonujemy operacje (zapis, odczyt)
+- zamykamy plik - moze byc tak, ze jak nie zamkniecie pliku, to pozniej inny program nie bedzie w stanie z niego skorzystac
 """
 
+
+file_handle = open('day_4_pgg/test.txt')
+content = file_handle.read()
+print(content)
+file_handle.close()
+
+print('-' * 60)
+
+file_handle = open('day_4_pgg/test.txt', 'w+')  # tryb do zapisu
+file_handle.write('Ala ma kota\nKot ma kompilator')
+file_handle.close()
+
+print('-' * 60)
+
+"""
+zeby nie trzeba bylo pamietac o zamykaniu pliku mozemy wykorzystac compound statement
+takie samo rozwiazanie mozemy zastowac nie tylko do plikwo, ale takze do innych zasobow
+np. requesty do API, zasoby sieciowe, itp.
+"""
+
+with open('day_4_pgg/test.txt', 'r+', encoding='utf-8') as file_handle:
+    content = file_handle.read()
+    print(content)
+    print(file_handle.closed)  # False
+    print(file_handle.encoding)  # UTF-8
+    print(file_handle.mode)  # r+
+    print(file_handle.name)  # day_4_pgg/test.txt
+    print(file_handle.writable())  # True
+    print(file_handle.readable())  # True
+
+print(file_handle.closed)  # True
+
+print('Jestem poza with')
