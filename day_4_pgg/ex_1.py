656d77fbe40db182f374539335c4aee9ab083e3e
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-27 10:17:58 +0100

    ex 1

---
+++ day_4_pgg/ex_1.py
@@ -0,0 +1,26 @@
+"""
+Utwórz następującą strukturę pakietów i modułów:
+- główny pakiet mathematica
+- pakiet wewnętrzny geometry
+- moduł figures w pakiecie geometry z funkcjami
+    - square_area oraz triangle_area
+- pakiet wewnętrzny algebra
+- moduł formulas w pakiecie algebra z funkcjami
+    - add, substract, multiply, divide
+
+Struktura katalogów i plików:
+mathematica/
+    algebra/
+        formulas.py
+    geometry/
+        figures.py
+
+W pliku z tym cwiczeniem policz: 
+- pole kwadratu o boku 5
+- podziel 10 przez 2
+"""
+
+# tutaj robimy importy:
+# from PAKIET.MODUL import cos
+# from mathematica.algebra.formulas import square_area
+# import PAKIET.MODUL
