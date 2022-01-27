bbe0d7df20a5dd2ff968368a54f5abba7f835bfc
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-27 15:29:47 +0100

    ex 3

--- day_4_pgg/ex_3.py
+++ day_4_pgg/ex_3.py
@@ -6,5 +6,27 @@ Wynikowy plik powinien zawierać unikalne, posortowane, poprawne adresy email.
 
 https://emailregex.com/
 https://regex101.com/ - edytor do wyrazen regularnych
+https://realpython.com/regex-python/
+https://realpython.com/regex-python-part-2/
+r"" - nie bedzie eskejpowal znakow w napisie - https://docs.python.org/3/reference/lexical_analysis.html#literals
 """
 
+import re
+
+file_name = 'day_4_pgg/emails.txt'
+
+emails = set()
+
+with open(file_name) as file_handle:
+    for line in file_handle:
+        if line.count('@') != 1:
+            continue
+
+        email_address = line.strip().lower().replace(' ', '')
+        if re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email_address):
+            emails.add(email_address)
+    
+sorted_emails = sorted(emails)
+
+with open('day_4_pgg/emails_cleaned.txt', 'w') as file_handle:
+    file_handle.write('\n'.join(sorted_emails))
