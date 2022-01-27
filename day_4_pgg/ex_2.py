feeaac84ab95ebc39d156f7f03031f63e74c3357
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-27 14:58:15 +0100

    ex 3

--- day_4_pgg/ex_2.py
+++ day_4_pgg/ex_2.py
@@ -5,3 +5,37 @@ Przykład użycia:
 1: pierwsza linia pliku
 2: druga linia pliku
 """
+
+file_name = 'day_4_pgg/emails.txt'
+
+with open(file_name) as file_handle:
+    # wersja 1 
+    line_number = 1
+    for line in file_handle:
+        # print(f'{line_number}: {line.rstrip()}')
+        print(f'{line_number}: {line[:-1]}')
+        line_number += 1 
+
+    print('---')
+
+    # wersja 2
+    file_handle.seek(0)
+    for line_number, line in enumerate(file_handle, start=1):
+        print(f'{line_number}: {line[:-1]}')
+
+    print('---')
+
+    # wersja 3
+    file_handle.seek(0)
+    line_number = 1
+    while True:
+        line = file_handle.readline()
+
+        if not line:
+            break
+
+        print(f'{line_number}: {line.rstrip()}')
+        line_number += 1
+
+
+
---
+++ day_4_pgg/ex_3.py
@@ -0,0 +1,10 @@
+"""
+Napisz program wczytujący listę adresów email z pliku i tworzący nowy plik z odfiltrowaną zawartością.
+Wejściowy plik może zawierać duplikaty adresów, ten sam adres pisany różną wielkością liter, 
+linie zawierające białe znaki oraz błędne adresy email (brak znaku @ lub występuje on wiele razy). 
+Wynikowy plik powinien zawierać unikalne, posortowane, poprawne adresy email.
+
+https://emailregex.com/
+https://regex101.com/ - edytor do wyrazen regularnych
+"""
+
