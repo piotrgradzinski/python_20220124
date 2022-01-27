4fe28e97fb75dcdfe8d0dd131aa667390c4799c9
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-27 11:53:15 +0100

    dates

--- day_4_pgg/dates.py
+++ day_4_pgg/dates.py
@@ -10,6 +10,7 @@ Bedziemy korzystali z:
 """
 
 import datetime
+from dateutil import tz, parser
 
 # tworzenie
 # date
@@ -34,7 +35,82 @@ Strefy czasowe (time zone)
 https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 """
 
-from dateutil import tz
+print(tz.gettz('America/St_Johns'))  # Mac/linux: /usr/share/zoneinfo/America/St_Johns
 
-print(tz.gettz('America/St_Johns'))
+dt = datetime.datetime.now()
+print(dt)
 
+dt = datetime.datetime.now(tz.gettz('America/St_Johns'))
+print(dt)
+print(dt.tzname())
+
+print(dt.year)
+print(dt.month)
+print(dt.day)
+print(dt.hour)
+print(dt.minute)
+print(dt.second)
+print(dt.weekday())  # Monday == 0 ... Sunday == 6
+print(dt.isoweekday())  # Monday == 1 ... Sunday == 7
+print(dt.isocalendar().week)
+
+# konwertowanie datetime na stringa
+# https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+# string format time
+print(dt.strftime('%d.%m.%Y %H:%I:%S'))
+print(dt.isoformat())  # 2022-01-27T06:51:17.022635-03:30
+
+dt = datetime.datetime.now(tz=datetime.timezone.utc)
+print(dt)
+print(dt.tzname())  # UTC
+
+dt = datetime.datetime.utcnow()  # lepiej uzywac datetime.datetime.now(tz=datetime.timezone.utc), https://docs.python.org/3/library/datetime.html#datetime.datetime.utcnow
+print(dt)
+print(dt.tzname())  # None
+
+
+dt = datetime.datetime.now(tz=tz.tzlocal())
+print(dt)
+print(dt.tzname())  # CET
+
+"""
+Wczytywanie dat
+timestamp - int, liczba sekund, jaka upynela od EPOCH do teraz
+EPOCH - w linuxach, to 1970-01-01 00:00:00, https://en.wikipedia.org/wiki/Epoch_(computing)
+epochconverter - https://www.epochconverter.com/
+kiedy operujemy na timestamp, to musimy wiedzec w jakiej strefie czasowej zostaly zapisane
+"""
+
+# wczytywanie z timestampa
+dt = datetime.datetime.fromtimestamp(1615890862)  
+print(dt)  # 2021-03-16 11:34:22
+
+dt = datetime.datetime.fromtimestamp(1615890862, tz=datetime.timezone.utc)
+print(dt)  # 2021-03-16 10:34:22+00:00
+
+# wczytywanie z ISO
+dt = datetime.datetime.fromisoformat("2022-01-27T06:51:17.022635-03:30")  # od 3.7
+print(dt)
+
+"""
+offset-naive datetime - bez strefy czasowej
+offset-aware datetime - ze strefa czasowa
+"""
+dt1 = datetime.datetime.now()
+dt2 = datetime.datetime.now(tz.gettz('America/St_Johns'))
+
+# dt3 = dt1 - dt2  # TypeError: can't subtract offset-naive and offset-aware datetimes
+
+# wczytywanie ze napisu
+# strptime - string parse time
+dt = datetime.datetime.strptime("27.01.2022 11:46:00", "%d.%m.%Y %H:%M:%S")
+print(dt)
+
+# dateutil i parser
+dt = parser.parse("27.01.2022 11:46:00")
+print(dt)
+
+dt = parser.parse("2022-01-27T06:51:17.022635-03:30")
+print(dt)
+dt2 = dt.astimezone(tz.gettz('Europe/Warsaw'))
+print(dt2)
