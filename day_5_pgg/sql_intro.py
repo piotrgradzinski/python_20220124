4330891c0bad2b32707bddbfdbc0df6b66d05457
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-28 13:00:22 +0100

    sql

---
+++ day_5_pgg/sql_intro.py
@@ -0,0 +1,15 @@
+"""
+SQL na przykladzie SQLite
+Baza testowa: https://www.sqlitetutorial.net/sqlite-sample-database/
+jej schemat: https://www.sqlitetutorial.net/wp-content/uploads/2018/03/sqlite-sample-database-diagram-color.pdf
+Diagramy ERD:
+- https://pl.wikipedia.org/wiki/Diagram_zwi%C4%85zk%C3%B3w_encji
+- https://www.guru99.com/er-diagram-tutorial-dbms.html
+
+Narzedzie do robienia diagramow (wszelakich): https://app.diagrams.net/
+Wtyczka SQLite do Visual Studio Code: 
+- SQLite (ta z piorkiem)
+- https://marketplace.visualstudio.com/items?itemName=alexcvzz.vscode-sqlite
+- po instalacji: ctrl+shift+p ->  SQLite: Open database
+"""
+
