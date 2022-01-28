fad5e5085ca2bf7d5c58408be57e0ba5fb82a8b4
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-28 10:15:56 +0100

    ex 2

--- day_5_pgg/ex_2_argv.py
+++ day_5_pgg/ex_2_argv.py
@@ -3,23 +3,34 @@ Dodajmy przekazywanie nazwy pliku do otwarcia z poziomu sys.argv.
 Niech program przyjmuje dwa argumenty: 
 - nazwe pliku do odczytania
 - nazwe pliku, do ktorego ma zapisac wynik
+
+uruchomienie: python ex_2_argv.py emails.txt emails_cleaned.txt
 """
 import re
+import sys
 
-file_name = 'day_5_pgg/emails.txt'
+try:
+    file_name = sys.argv[1]
+    output_file_name = sys.argv[2]
 
-emails = set()
+    emails = set()
 
-with open(file_name) as file_handle:
-    for line in file_handle:
-        if line.count('@') != 1:
-            continue
+    with open(file_name) as file_handle:
+        for line in file_handle:
+            if line.count('@') != 1:
+                continue
 
-        email_address = line.strip().lower().replace(' ', '')
-        if re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email_address):
-            emails.add(email_address)
-    
-sorted_emails = sorted(emails)
+            email_address = line.strip().lower().replace(' ', '')
+            if re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email_address):
+                emails.add(email_address)
+        
+    sorted_emails = sorted(emails)
 
-with open('day_5_pgg/emails_cleaned.txt', 'w') as file_handle:
-    file_handle.write('\n'.join(sorted_emails))
+    with open(output_file_name, 'w') as file_handle:
+        file_handle.write('\n'.join(sorted_emails))
+except IndexError:
+    print('Usage: python ex_2_argv.py SOURCE_FILE OUTPUT_FILE')
+    exit(1)
+except FileNotFoundError:
+    print(f'File not found.')
+    exit(1)
