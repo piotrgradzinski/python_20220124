fc570e2eec00d54180770992a95ed6f2ed1d00e6
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-28 09:38:50 +0100

    ex 1

--- day_5_pgg/ex_1.py
+++ day_5_pgg/ex_1.py
@@ -5,3 +5,30 @@ Napisz program, który wczyta pracowników z pliku employees.xlsx i policzy:
 
 Dane wyświetl w konsoli i zapisz do pliku employees_report.json.
 """
+
+from openpyxl import load_workbook
+import json
+
+wb = load_workbook('day_5_pgg/employees.xlsx')
+ws = wb.active
+
+# salaries = []
+# for row in ws.iter_rows(min_row=2):
+    # salaries.append(row[2].value)
+
+salaries = [row[2].value for row in ws.iter_rows(min_row=2)]
+
+print(salaries)
+
+salaries_sum = sum(salaries)
+salaries_mean = salaries_sum / len(salaries)
+
+print(f'Salaries sum: {salaries_sum:.2f}')
+print(f'Salaries mean: {salaries_mean:.2f}')
+
+with open('day_5_pgg/employees_report.json', 'w') as file_handle:
+    json_data = {
+        'salaries_sum': salaries_sum,
+        'salaries_mean': salaries_mean,
+    }
+    json.dump(json_data, file_handle)
