600dd9528d9dd11233bf24c54ccb0a34115ed475
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2022-01-28 09:14:28 +0100

    ex 5

---
+++ day_5_pgg/ex_1.py
@@ -0,0 +1,7 @@
+"""
+Napisz program, który wczyta pracowników z pliku employees.xlsx i policzy:
+- sumę wynagrodzeń
+- średnia wynagrodzeń
+
+Dane wyświetl w konsoli i zapisz do pliku employees_report.json.
+"""
